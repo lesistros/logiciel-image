c3c002362cef833d2b3f474bd50df1fe05e9106a
lesistros <stuyck.f@gmail.com>
2017-01-17 16:27:22 +0100

    update 17/01

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,14 @@
 #include <string>
 #include <stdlib.h>
 #include <sstream>
+#include <algorithm> 
 
 #include "opencv2/core.hpp"
 #include "opencv2/face.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/objdetect.hpp"
+
 
 
 using namespace cv;
@@ -61,23 +63,23 @@ int main( int argc, char** argv)
 		//read max value
 		getline(myfile , line);
 		maxColor = atoi ( line.c_str());
-		
+		/*
 		cout << "Type du fichier : " << filetype << "\n";
 		cout << "Taille : " << width << " x " << height << " x 3 "<<"\n";
 		cout << "Max scale : " << maxColor << "\n\n\n";
-
+		*/
 
 // remise sour la forme des formats PGM pour afficher (pas obligatoire)
 
-	RED <<	"P2" << "\n";
+	RED <<	"P2" << "\n"; //p2 pour le noir et blanc pour afficher avec open cv
 	RED <<	width << " " << height <<"\n";
 	RED <<	maxColor << "\n";
 
-	GREEN << filetype << "\n";
+	GREEN << "P2" << "\n";
 	GREEN << width << " " << height <<"\n";
 	GREEN << maxColor << "\n";
 
-	BLUE << filetype << "\n";
+	BLUE << "P2" << "\n";
 	BLUE << width << " " << height <<"\n";
 	BLUE << maxColor << "\n";
 
@@ -86,7 +88,30 @@ int main( int argc, char** argv)
 	// creation d une matrice pour un futur traitement
 
 	int MatriceImg[height-1][width-1][3];
+	
+	
 
+	
+
+
+
+
+
+	int HistRED[255];
+	int HistGREEN[255];
+	int HistBLUE[255];
+
+	int l=0;
+// TEST
+
+for(l=0; l<255; l++)
+{
+	HistRED[l] = 0;
+	HistGREEN[l] =0;
+	HistBLUE[l] =0;
+	
+}
+	
 
     
     // Inserting the values into the test array
@@ -124,6 +149,12 @@ int main( int argc, char** argv)
 				//cout << i<<" ";
 				i++;
 
+				// TEST Histogramme
+				HistRED[pixel+256]=HistRED[pixel+256]+1;
+
+
+				//HIST.at<float>(0,pixel+256) = HIST.at<float>(0,pixel+256) +1;
+
 				}
 			else if(compteur == 2 )
 				{
@@ -132,6 +163,8 @@ int main( int argc, char** argv)
 				MatriceImg[i][j][2] << pixel+256;
 				i++;
 				compteur++;
+				HistGREEN[pixel+256]=HistGREEN[pixel+256]+1;
+
 
 				}
 			else
@@ -142,6 +175,10 @@ int main( int argc, char** argv)
 				i++;
 				compteur = 1;
 				compteur_col ++;
+				HistBLUE[pixel+256]=HistBLUE[pixel+256]+1;
+				
+				//cout << HistBLUE[pixel+256];
+
 
 				}
 			}
@@ -159,22 +196,31 @@ int main( int argc, char** argv)
  				//cout << "colonne : "<< compteur_col << "\n"
 				compteur++;
 				i++;
+				HistRED[pixel]=HistRED[pixel]+1;
+
+				//HIST.at<float>(0,pixel) = HIST.at<float>(0,pixel) +1;
+
 
 				}
 			else if(compteur == 2 )
 				{
+				
 				MatriceImg[i][j][2] << pixel;
 				GREEN << pixel  << " ";
 				i++;
 				compteur++;
+				HistGREEN[pixel]=HistGREEN[pixel]+1;
 				}
 			else
 				{
+				
 				MatriceImg[i][j][3] << pixel;
 				BLUE << pixel  << " ";
 				compteur = 1;
 				compteur_col ++;
 				i++;
+				HistBLUE[pixel]=HistBLUE[pixel]+1;
+				//cout << HistBLUE[pixel];
 
 				}
 			}
@@ -190,11 +236,79 @@ int main( int argc, char** argv)
 				j=j+1;
 				
 				}
+			
+		}
+		cout << "ca bug pas avant "<< "\n";
+		cout << "ca bug pas avant "<< "\n";
+	
+//HIST.at<float>(0,1) = 5000;
+		
+// affichage matrice histogramme
+/*
+HIST.at<float>(0,1) = 5000;
+HIST.at<float>(120,80) = 120;
+HIST.at<float>(1100,90) = 1100;
+HIST.at<float>(1150,91) = 1150;
+HIST.at<float>(1151,90) = 1100;
+HIST.at<float>(1152,91) = 10150;
+*/
+//int A;		
+ //A << max(Histogramme[]) ;
+
+cout << "ca bug pas avant "<< "\n";
+cout << "ca bug pas avant "<< "\n";
+
+Mat HistR(256,256,CV_32FC1,0.0f);
+Mat HistG(256,256,CV_32FC1,0.0f);
+//Mat HistB(256,256,CV_32FC1,0.0f);
+		
+	
+ 
+
+
+		int n = 0;	
+		for (n=0; n<255; n++)
+		{
+			
+			
+			
+			HistR.at<float>(256-HistRED[n]   ,n) = HistRED[n];
+			HistG.at<float>(256-HistGREEN[n] ,n) = HistGREEN[n];
+			//HistB.at<float>(256-HistBLUE[n]  ,n) = HistBLUE[n];
+			/*			
+			cout << "\n";
+
+			cout << Histogramme[n] << " la ";
+			cout << "\n";
+
+			cout << n << " prout ";
+			//Histogramme[n];
+			*/
 		}
 
-		
+
+
+		//cout << "\n" << HIST << endl;
+		// Affichage avec openCV		
+
 		myfile.close();
 		Mat image;
+		
+		//Mat Hist=Histogramme;		
+
+  		namedWindow( "HISTOGRAMME ROUGE",  WINDOW_AUTOSIZE  );    
+		imshow( "HISTOGRAMME ROUGE ", HistR );
+		waitKey(0);
+		namedWindow( "HISTOGRAMME GREEN",  WINDOW_AUTOSIZE  );    
+		imshow( "HISTOGRAMME GREEN ", HistG );
+		waitKey(0);
+		//namedWindow( "HISTOGRAMME BLUE", 1256 );    
+		//imshow( "HISTOGRAMME BLUE ", HistB );
+ 		//waitKey(0);
+    		
+
+
+		//cout << histImg;
 		image = imread("../test/ImageEnCoursDeTraitement/RED.pgm");
 		//cout << image;
 		namedWindow( "Display window", WINDOW_AUTOSIZE );   // Create a window for display.
